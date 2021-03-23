74edcfffe3e5be55ac86e26c62a8fb9f5d10b1aa
Calum Dingwall <24cdingwall@gmail.com>
2021-03-23 13:53:44 -0400

    fix

--- src/components/themepicker.js
+++ src/components/themepicker.js
@@ -20,7 +20,7 @@ export default function ThemePicker() {
 		<>
 			{myThemes.map((item, index) => {
 				const nextTheme = myThemes.length -1 === index ? myThemes[0].id : myThemes[index + 1].id;
-			
+
 				return item.id === theme ? (
 					<span className="material-icons" style={{'cursor': 'pointer', 'user-select': 'none'}} key={item.id} aria-label={`${item.name}`} onClick={() => switchTheme(nextTheme)}>
 						{item.icon}
