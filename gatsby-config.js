8293915b0eaf888006e2916319460b5194eec77d
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-05-21 20:17:33 -0400

    random changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,59 +1,66 @@
 module.exports = {
-  siteMetadata: {
-    title: `Somerset County 4-H Fair`,
-    description: `The Somerset County 4-H Fair App/Website`,
-    author: `Somerset County 4-H`
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Somerset County 4-H Fair`,
-        short_name: `SC 4-H Fair`,
-        start_url: `/`,
-        background_color: `#EDEDED`,
-        theme_color: `#009959`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
-    `gatsby-plugin-sass`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+	siteMetadata: {
+		title: `Somerset County 4-H Fair`,
+		description: `The Somerset County 4-H Fair App/Website`,
+		author: `Somerset County 4-H`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-image`,
 		{
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /\.inline\.svg$/,
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-theme-switcher',
-      options: {
-        defaultDarkTheme: 'theme-dark',
-        defaultLightTheme: 'theme-light',
-      },
-    },
-  ],
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Somerset County 4-H Fair`,
+				short_name: `SC 4-H Fair`,
+				start_url: `/`,
+				background_color: `#EDEDED`,
+				theme_color: `#009959`,
+				display: `standalone`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-plugin-gatsby-cloud`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images/`,
+			},
+		},
+		`gatsby-plugin-sass`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+		{
+			resolve: 'gatsby-plugin-react-svg',
+			options: {
+				rule: {
+					include: /\.inline\.svg$/,
+				},
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-theme-switcher',
+			options: {
+				defaultDarkTheme: 'theme-dark',
+				defaultLightTheme: 'theme-light',
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: ['G-QYEGYTKG7B'],
+			},
+		},
+	],
 }
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,24 +1,26 @@
-import React from "react"
-import Layout from "../components/layout"
-import Header from "../components/header"
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react"
+import React from 'react'
+import Layout from '../components/layout'
+import Header from '../components/header'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 const MapPage = ({ google }) => {
-  return (
-    <div style={{ height: "100%" }}>
-      <Header offsetContent={false} containerStyle={{ height: "100%" }} />
-      <Map google={google} zoom={14}>
-        {/* <Marker onClick={this.onMarkerClick} name={"Current location"} />
+	return (
+		<div style={{ height: '100%' }}>
+			<Header offsetContent={false} containerStyle={{ height: '100%' }} />
+			<Map google={google} zoom={14}>
+				<Marker name={'Current location'} />
+				{/* <Marker onClick={this.onMarkerClick} name={'Current location'} /> */}
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow> */}
-      </Map>
-    </div>
-  )
+				<InfoWindow>
+					{/* <InfoWindow onClose={this.onInfoWindowClose}> */}
+					<div>
+						<h1>{this.state.selectedPlace.name}</h1>
+					</div>
+				</InfoWindow>
+			</Map>
+		</div>
+	)
 }
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyCMSL0SaWn10CCGkILYDmFLz-DntlTvWus",
+	apiKey: 'AIzaSyCMSL0SaWn10CCGkILYDmFLz-DntlTvWus',
 })(MapPage)
