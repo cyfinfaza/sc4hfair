40b0aa6220f1d8e48c6249a8334a2d49eb5c32b9
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-05-29 23:16:44 -0400

    created feedback page

---
+++ src/pages/feedback.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+// import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
+import * as pageStyle from './feedback.module.css'
+
+// @todo: add contact form
+export default function feedback() {
+	return (
+		<Layout title="Feedback">
+			<h1>Send Feedback</h1>
+			<h2>Name</h2>
+			<p></p>
+			<h2>Email</h2>
+			<p></p>
+			<h2>Name</h2>
+			<p></p>
+		</Layout>
+	)
+}
