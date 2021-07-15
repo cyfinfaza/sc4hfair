0119386fa0961bfec2ed8b7701f377851fe76735
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-07-15 13:43:34 -0400

    added cypress testing suite

---
+++ cypress/integration/test-test-wow.js
@@ -0,0 +1,9 @@
+describe("A test for testing the testing software's test capability for testing software", () => {
+	it('is sane', ()=>{
+		cy.visit('/')
+		expect(true).to.equal(true)
+	})
+	it('is not python', ()=>{
+		expect([1, 2, 3]).not.to.equal([1, 2, 3])
+	})
+})
