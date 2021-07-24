75635be55923d83cb3337545487aeb7b9f60fc6d
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2021-07-24 13:59:37 -0800

    more specific policy

--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -2,31 +2,46 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 
-// @todo: actually write it
-// we collect name and email when interest manager and then send it to clubs
 export default function privaceePolicee() {
 	return (
 		<Layout title="Privacy Policy">
 			<p>
-				Your privacy is important to us. It is Somerset County 4H Fair's policy
-				to respect your privacy regarding any information we may collect from
-				you across our website, <Link to="/">sc4hfair</Link>.
+				Your privacy is important to us. It is Somerset County 4H Fair App's
+				policy to respect your privacy regarding any information we may collect
+				from you across our website, <Link to="/">sc4hfair</Link>.
 			</p>
 			<p>
-				We only ask for personal information when we truly need it to provide a
-				service to you. We collect it by fair and lawful means, with your
-				knowledge and consent. We also let you know why we’re collecting it and
-				how it will be used.
+				We only ask for personal information when we need it to provide services
+				to you. We collect it by fair and lawful means, with your knowledge and
+				consent. We also let you know why we’re collecting it and how it will be
+				used.
 			</p>
 			<p>
-				We only retain collected information for as long as necessary to provide
-				you with your requested service. What data we store, we’ll protect
-				within commercially acceptable means to prevent loss and theft, as well
-				as unauthorized access, disclosure, copying, use or modification.
+				If you create an account, your name, email address, and profile picture
+				(from the external account you use to create it) will be stored. Using
+				multiple external accounts with the same email address will only use one
+				account. Additional information may be managed in the{' '}
+				<Link to="/settings">user settings</Link>.
+			</p>
+			<p>
+				Viewing any pages on the website will automatically collect analytical
+				data. This data consists of the timestamp, URL of the page, browser user
+				agent, when user first viewed a page, and how many pages the user has
+				viewed, all stored under a tracking ID. The tracking ID is unique to
+				your browser session and does not contain any traceable information like
+				your IP address.
+			</p>
+			<p>
+				We’ll protect any data we store within commercially acceptable means to
+				prevent loss and theft, as well as unauthorized access, disclosure,
+				copying, use or modification.
 			</p>
 			<p>
 				We don’t share any personally identifying information publicly or with
-				third-parties, except when required to by law.
+				third-parties besides Somerset County 4H, except when required to by
+				law. If you use the <Link to="/interests">club interest manager</Link>,
+				your name, email address, and other relavent information will be shared
+				with leaders of the clubs you are interested in.
 			</p>
 			<p>
 				Our website may link to external sites that are not operated by us.
@@ -45,7 +60,7 @@ export default function privaceePolicee() {
 				questions about how we handle user data and personal information, feel
 				free to <Link to="/feedback">contact us</Link>.
 			</p>
-			<p>This policy is effective as of 29 May 2021.</p>
+			<p>This policy was last updated and effective as of July 24, 2021.</p>
 		</Layout>
 	)
 }
