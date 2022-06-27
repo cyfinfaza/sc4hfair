09eef1dc60dc0659d29761508107ccd82a00bcae
Cy <42043351+cyfinfaza@users.noreply.github.com>
2022-06-27 10:33:55 +0000

    fix vercel git branch name issue

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -153,7 +153,8 @@ export default function SettingsPage({ data }) {
 			</p>
 			<div style={{ opacity: 0.5 }}>
 				<code onClick={_ => setShowingAdditionalBuildInfo(true)}>
-					{data.gitBranch.name}/{data.gitBranch.commit}
+					{process.env.VERCEL_GIT_COMMIT_REF || data.gitBranch.name}/
+					{data.gitBranch.commit}
 				</code>
 				{showingAdditionalBuildInfo && (
 					<>
