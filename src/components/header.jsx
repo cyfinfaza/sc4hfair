da7ef2f4fe74cffc728b3818890f25b2072f4aa1
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2022-07-28 14:26:34 -0400

    change status bar color and disable interest list when offline

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,7 @@ import ThemePicker from './themepicker'
 import LinkButton from './linkbutton'
 import SponsorSpot from './sponsorSpot'
 import * as buttonStyle from './button.module.css'
+import { Helmet } from 'react-helmet'
 // import { graphql, useStaticQuery } from 'gatsby'
 
 const Header = ({ offsetContent = true }) => {
@@ -32,6 +33,9 @@ const Header = ({ offsetContent = true }) => {
 			} else {
 				document.body.style.setProperty('--status-bar', '#888888')
 			}
+			document
+				.querySelector('meta[name="theme-color"]')
+				.setAttribute('content', getComputedStyle(document.body).getPropertyValue('--status-bar'))
 		},
 		[isOnline]
 	)
@@ -44,102 +48,112 @@ const Header = ({ offsetContent = true }) => {
 	// 	}
 	// `)
 	return (
-		<div className={headerStyle.invisible} style={offsetContent ? {} : { height: 0 }}>
-			<div
-				className={headerStyle.visible}
-				style={{
-					height: menuOpen ? '100vh' : null,
-					background: isOnline ? 'var(--navbar)' : 'var(--navbar-grey)',
-				}}
-			>
-				<div className={headerStyle.topBar}>
-					<Logo
-						className={headerStyle.headerLogo}
-						onClick={() => {
-							window.location.href = '/'
-						}}
-						style={{ cursor: 'pointer' }}
-					/>
-					<div className="horizPanel2" style={{ gap: '12px' }}>
-						{!isOnline && <span className="material-icons">cloud_off</span>}
-						<button
-							className={`${headerStyle.menuButton} ${buttonStyle.button}`}
-							onClick={toggleMenu}
-						>
-							<div className={headerStyle.menuIconContainer}>
-								<i
-									className="material-icons"
-									style={!menuOpen ? {} : { transform: 'scale(0.6)', opacity: 0 }}
-								>
-									menu
-								</i>
-								<i
-									className="material-icons"
-									style={menuOpen ? {} : { transform: 'scale(0.6)', opacity: 0 }}
-								>
-									close
-								</i>
-							</div>
-							<span className={headerStyle.menuButtonLabel}>Menu</span>
-						</button>
-					</div>
-				</div>
-				<div className={headerStyle.menuArea}>
-					<div className={headerStyle.menuGrid}>
-						<LinkButton label="Latest" icon="home" linksTo="/" big opaque={false} />
-						<LinkButton label="Map" icon="map" linksTo="/map" big opaque={false} />
-						<LinkButton label="Schedule" icon="event_note" linksTo="/schedule" big opaque={false} />
-						<LinkButton label="Clubs" icon="groups" linksTo="/clubs" big opaque={false} />
-						<LinkButton
-							label="Interest List"
-							icon="list_alt"
-							linksTo="/interests"
-							big
-							opaque={false}
+		<>
+			<Helmet meta={[{ name: 'theme-color' }]} />
+			<div className={headerStyle.invisible} style={offsetContent ? {} : { height: 0 }}>
+				<div
+					className={headerStyle.visible}
+					style={{
+						height: menuOpen ? '100vh' : null,
+						background: isOnline ? 'var(--navbar)' : 'var(--navbar-grey)',
+					}}
+				>
+					<div className={headerStyle.topBar}>
+						<Logo
+							className={headerStyle.headerLogo}
+							onClick={() => {
+								window.location.href = '/'
+							}}
+							style={{ cursor: 'pointer' }}
 						/>
-						<LinkButton
-							label="Scavenger Hunt"
-							icon="travel_explore"
-							linksTo="/scavenger-hunt"
-							big
-							opaque={false}
-						/>
+						<div className="horizPanel2" style={{ gap: '12px' }}>
+							{!isOnline && <span className="material-icons">cloud_off</span>}
+							<button
+								className={`${headerStyle.menuButton} ${buttonStyle.button}`}
+								onClick={toggleMenu}
+							>
+								<div className={headerStyle.menuIconContainer}>
+									<i
+										className="material-icons"
+										style={!menuOpen ? {} : { transform: 'scale(0.6)', opacity: 0 }}
+									>
+										menu
+									</i>
+									<i
+										className="material-icons"
+										style={menuOpen ? {} : { transform: 'scale(0.6)', opacity: 0 }}
+									>
+										close
+									</i>
+								</div>
+								<span className={headerStyle.menuButtonLabel}>Menu</span>
+							</button>
+						</div>
 					</div>
-					<div className={headerStyle.menuBottom}>
-						<ThemePicker navbar />
-						<LinkButton
-							label="Settings"
-							icon="settings"
-							linksTo="/settings"
-							opaque={false}
-							lightFont
-						/>
-						<LinkButton
-							label="About 4-H"
-							icon={
-								<FourH
-									style={{
-										height: '100%',
-										// fill: 'var(--navbar-text)',
-										fill: 'var(--text)',
-										transition: 'fill var(--theme-transition)',
-									}}
-								/>
-							}
-							href="https://4histops.org"
-							opaque={false}
-							lightFont
-						/>
-					</div>
-					<div className={headerStyle.sponsorArea}>
-						<SponsorSpot />
-					</div>
-					{/* <code className={headerStyle.quickBuildInfo}>
+					<div className={headerStyle.menuArea}>
+						<div className={headerStyle.menuGrid}>
+							<LinkButton label="Latest" icon="home" linksTo="/" big opaque={false} />
+							<LinkButton label="Map" icon="map" linksTo="/map" big opaque={false} />
+							<LinkButton
+								label="Schedule"
+								icon="event_note"
+								linksTo="/schedule"
+								big
+								opaque={false}
+							/>
+							<LinkButton label="Clubs" icon="groups" linksTo="/clubs" big opaque={false} />
+							<LinkButton
+								label="Interest List"
+								icon="list_alt"
+								linksTo="/interests"
+								big
+								opaque={false}
+								disabled={!isOnline}
+							/>
+							<LinkButton
+								label="Scavenger Hunt"
+								icon="travel_explore"
+								linksTo="/scavenger-hunt"
+								big
+								opaque={false}
+							/>
+						</div>
+						<div className={headerStyle.menuBottom}>
+							<ThemePicker navbar />
+							<LinkButton
+								label="Settings"
+								icon="settings"
+								linksTo="/settings"
+								opaque={false}
+								lightFont
+							/>
+							<LinkButton
+								label="About 4-H"
+								icon={
+									<FourH
+										style={{
+											height: '100%',
+											// fill: 'var(--navbar-text)',
+											fill: 'var(--text)',
+											transition: 'fill var(--theme-transition)',
+										}}
+									/>
+								}
+								href="https://4histops.org"
+								opaque={false}
+								lightFont
+							/>
+						</div>
+						<div className={headerStyle.sponsorArea}>
+							<SponsorSpot />
+						</div>
+						{/* <code className={headerStyle.quickBuildInfo}>
 						{metadata.gitBranch.name}/{metadata.gitBranch.commit}
 					</code> */}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 export default Header
