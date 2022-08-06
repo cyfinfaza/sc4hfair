19bb763172dfc7b247f19c71dad267719db655a2
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2022-08-06 01:12:31 -0400

    normalize diatrics in search

--- src/logic/search.js
+++ src/logic/search.js
@@ -1,30 +1,25 @@
 /* eslint-disable no-eval */
 
 export function exactSearch(objList, priorityKey, secondaryKeys, query) {
-	query = query.toLowerCase()
+	function prepare(text) {
+		return text
+			.toLowerCase()
+			.normalize('NFKD')
+			.replace(/[\u0300-\u036f]/g, '')
+	}
+	query = prepare(query)
 	if (query) {
 		// search for exact match, returning first objects that match primary key, then secondary key
 		return objList
 			.filter(obj => {
-				return (
-					eval('obj.' + priorityKey)
-						.toLowerCase()
-						.indexOf(query) !== -1
-				)
+				return prepare(eval('obj.' + priorityKey)).indexOf(query) !== -1
 			})
 			.concat(
 				objList.filter(obj => {
 					return (
 						secondaryKeys.some(key => {
-							return (
-								eval('obj.' + key)
-									.toLowerCase()
-									.indexOf(query) !== -1
-							)
-						}) &&
-						eval('obj.' + priorityKey)
-							.toLowerCase()
-							.indexOf(query) === -1
+							return prepare(eval('obj.' + key)).indexOf(query) !== -1
+						}) && prepare(eval('obj.' + priorityKey)).indexOf(query) === -1
 					)
 				})
 			)
